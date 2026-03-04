c752c556d1dc3ffb418527f5da2c1f1c0644df18
Igbana <igbanaaondosoo13@gmail.com>
2026-03-04 23:35:11 +0100

    First commit

---
+++ android/app/src/main/kotlin/com/example/bmdmusic/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bmdmusic
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'views/homepage.dart';
+
+void main() {
+  runApp(const MaterialApp(home: Homepage(),));
+}
