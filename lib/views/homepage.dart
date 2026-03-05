9c2bce8879a51e762c9cdb625fb3aac4bf5be95d
Igbana <igbanaaondosoo13@gmail.com>
2026-03-05 01:44:16 +0100

    Image Test

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -6,7 +6,20 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("BMD Music Home", style: Theme.of(context).textTheme.bodyLarge,),),
+      body: Center(child: Column(
+        children: [
+          Text("BMD Music Home", style: Theme.of(context).textTheme.bodyLarge,),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: .circular(12)),
+              child: Row(
+                children: [
+                  Image.asset('Reggae.png', cacheWidth: 100,)
+                  ],
+                  ),
+                  )
+        ],
+      ),),
     );
   }
 }
